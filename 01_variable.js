a02c42d52216832ce0efcc3d2ca01fcc463857e5
Rakshya-Panday <rakshyapanday202@gmail.com>
2023-10-17 22:57:52 +0545

    variable and datatypes in javascript

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const id = 5
+let name = "rakshya"
+var password = "12334"
+city = "kathmandu" //this is also valid way but it is used.
+let state ; // this show the undefined output.
+let email = "" // this show the null output.
+
+// id = 6 // we cannot change the value of constant.
+name = "apekshya"
+password = "7899"
+city = "tamghas"
+//not to used var used let instead of var beacause of the scope and functional scope of the variable now we can use let.
+console.table([id,name,password,city,state,email])
+console.log(id)
+
---
+++ 03_conversionOperation.js
@@ -0,0 +1,14 @@
+let score = "33abc"
+//there are two ways to check the datatype of variable 1. typeOf and another is write a method means write inside ()
+console.log(typeof score)
+console.log(typeof(score))
+let chgNumber = Number(score);
+//we can convert a number to string or any other datatypes using Number String Booelan etc remeber that we can use capital ways not a number
+// n this show NaN output as chg that that of string into number we cannot easily convert that type od datatypes rember that.
+console.log(typeof chgNumber);
+console.log(chgNumber)
+//33=>33
+//"33an"=>NaN => not a number
+//true/false => 1/0
+//null = object
+//undefined = undefined
